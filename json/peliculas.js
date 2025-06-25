4505216425a55049e9cfa8c2371501f0d9913fdd
dalessandrolucas <dalessandrolucas23@gmail.com>
2025-06-25 17:26:06 -0300

    vistasPeliculas

---
+++ json/peliculas.js
@@ -0,0 +1,30 @@
+const peliculas = [
+    {
+        "titulo": "Shrek",
+        "imagen": "assets/shrek.jpg"
+    },
+    {
+        "titulo": "Trolls",
+        "imagen": "assets/Trolls.webp"
+    },
+    {
+        "titulo": "Lluvia de Hamburguesas 2",
+        "imagen": "assets/LluviaDeHamburguesas2.webp"
+    },
+    {
+        "titulo": "Hotel Transylvania",
+        "imagen": "assets/Hotel Transylvania.webp"
+    },
+    {
+        "titulo": "Mi Villano Favorito 3",
+        "imagen": "assets/MVF3.jpg"
+    },
+    {
+        "titulo": "Shrek Tercero",
+        "imagen": "assets/Shrek3.webp"
+    },
+    {
+        "titulo": "Shrek 2",
+        "imagen": "assets/img-shrek2.jpg"
+    }
+]
